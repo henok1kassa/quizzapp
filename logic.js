c46451ddbfd4621527d486b6dc1b62b7904c8435
jonakapu <jonakapu2@gmail.com>
2025-02-18 20:17:03 +0200

    finish logic

--- logic.js
+++ logic.js
@@ -1,4 +1,4 @@
-let indexQuestion = -1; // Current index of question we are on
+let indexQuestion = 0; // Current index of question we are on
 // let timer = 0;  // A clock that goes up by 1 every second
 let timeLastSubmit = 0;  // the time we last submitted an answer
 let timeExpired = false;  // Did time expire for current question?
@@ -20,41 +20,79 @@ let QUESTIONS = [
     },
     {
         "question": "Which continent does the south pole reside?",
-        "choices": ["Africa", "Antarctica", "Asia", "Australia", "Europe", "North America", "South America"],
+        "choices": ["Africa", "Antarctica", "Asia", "Australia"],
         "answer": 1
     },
     {
-        "question": "Which continent does the south pole reside?",
-        "choices": ["Africa", "Antarctica", "Asia", "Australia", "Europe", "North America", "South America"],
+        "question": "what is my name?",
+        "choices": ["henok", "gideon", "manashe", "joshua"],
         "answer": 1
     },
     {
-        "question": "Which continent does the south pole reside?",
-        "choices": ["Africa", "Antarctica", "Asia", "Australia", "Europe", "North America", "South America"],
+        "question": "how old is gideon?",
+        "choices": ["10", "45", "80", "too old"],
         "answer": 1
     },
 ];
 
 function progressBarHandler() {
-    ++indexQuestion
     const progressBar = document.getElementById('score')
-    progressBar.textContent = `${indexQuestion + 1} / ${QUESTIONS.length}`
+    progressBar.textContent = `${indexQuestion + 1 } / ${QUESTIONS.length}`
     if (indexQuestion === QUESTIONS.length) {
         alert("hi you finshed the quizz wait for the results")
     }
 }
 progressBarHandler()
 
-//  added more code!
-
 // next question handler:
 const myBtn = document.getElementById('myBtn');
 function clickToNextQuestion() {
-
+    indexQuestion += 1 
     progressBarHandler()
 }
 myBtn.addEventListener('click', clickToNextQuestion);
 
+// added more code!
+// questions update:
+
+function emptyElementItems(element){
+    while(element.firstChild){
+        element.removeChild(element.firstChild);
+    }
+}
+const questionElement = document.getElementById("question");
+
+    const showOptionsContainer = document.querySelector(".options")
+// Function to show the current question
+function showQuestion() {
+    questionElement.textContent = QUESTIONS[indexQuestion].question;
+    emptyElementItems(showOptionsContainer)
+    QUESTIONS[indexQuestion].choices.forEach( (currentAnswer,i) => {
+        const answer = createQuestionAnswer(currentAnswer,i + 1)
+
+        showOptionsContainer.append(answer)
+        answer.addEventListener('click', () => buttonColorChange(`.op${i + 1}`))
+    })
+}
+function createQuestionAnswer(answer,index){
+    const button = document.createElement("button")
+    const text = document.createElement("div")
+    const mark = document.createElement("div")
+
+    text.textContent= answer
+
+    text.classList.add(`text${index}`)
+    button.classList.add(`op${index}`)
+    mark.classList.add("mark")
+   
+
+    button.append(text, mark)
+    return button
+}
+myBtn.addEventListener("click", function () {
+    showQuestion();
+});
+showQuestion()
 // changing the button color when clickd 
 let isButtonClicked = false
 let lastAnswer = null
@@ -77,19 +115,9 @@ function buttonColorChange(cl) {
 // reseting the answer 
 function resetPrevAnswer() {
     let lastAnswerElement = document.querySelector(lastAnswer)
-    lastAnswerElement.classList.toggle('active')
+    lastAnswerElement.classList.remove('active')
 }
 
-op1.addEventListener('click', () => buttonColorChange(".op1"))
-
-const op2 = document.querySelector(".op2");
-op2.addEventListener('click', () => buttonColorChange(".op2"))
-
-const op3 = document.querySelector(".op3");
-op3.addEventListener('click', () => buttonColorChange(".op3"))
-
-const op4 = document.querySelector(".op4");
-op4.addEventListener('click', () => buttonColorChange(".op4"))
 
 // timer
 //  progressBar.textContent = `${indexQuestion + 1} / ${QUESTIONS.length}`
